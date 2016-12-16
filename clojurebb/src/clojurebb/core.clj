dff899274134c7a10cda450870405c3a860427d7
Kleber Loayza <kloayza23@gmail.com>
2016-12-16 12:36:12 -0500

    se agrego la nueva funcionalidad

--- clojurebb/src/clojurebb/core.clj
+++ clojurebb/src/clojurebb/core.clj
@@ -4,29 +4,36 @@
 (defn ball  [speed direction]	
 	(def xdelta (* speed (Math/cos (Math/toRadians direction))))	
 	(def ydelta (* (- speed) (Math/sin (Math/toRadians direction) )))			
+	(list xdelta ydelta)
 )
 (defn move [x y xdelta ydelta]
  (def xr (+ x xdelta))
- (def yr (+ y ydelta))   
+ (def yr (+ y ydelta))    
+ (clojure.string/join ", " [xr yr])
 )
 (defn reflectHorizontal [xdelta]
-	(def xdelta (- xdelta 1))
+	(def xdeltar (- xdelta 1))
+	(+ xdeltar 0) 
 )
 (defn reflectVertical [ydelta]
-	(def ydelta (- ydelta 1))	
+	(def ydeltar (- ydelta 1))	
+	(+ ydeltar 0)
 )	
 (defn Container [x y width heigth]
 	(def x2 (+ x (- width 1)))
 	(def y2 (+ y (- heigth 1)))
+	(list x2 y2)
 )
 (defn collidesWith[x y radius x1 x2 y1 y2 xdelta ydelta]
 	(if 
 		(or (<= (- x radius) x1) (>= (+ x radius) x2))
-			(reflectHorizontal [xdelta] )
+			(reflectHorizontal xdelta)
+			(+ 0 0)
 	)
 	(if 
 		(or (<= (- y radius) y1) (>= (+ y radius) y2))
-			(reflectVertical [ydelta] )
+			(reflectVertical ydelta)
+			(+ 0 0)
 	)
 )	
 (defn -main
@@ -38,6 +45,16 @@
   (def x2 0)
   (def y2 0)
   (def xdelta 0)
-  (def ydelta 0)  
-  (move 4 5 3 3)  
+  (def ydelta 0)    
+  (loop [x 0]
+      (when (< x 10)
+         
+      	(move)
+
+
+        (recur (+ x 1))
+      )
+   )
+
+
 )
