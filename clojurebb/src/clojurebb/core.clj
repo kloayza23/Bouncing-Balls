6f04062af35f66c00a6c6307208dad4e58911c8e
kloayza23 <kloayza23@gmail.com>
2016-12-16 11:31:49 -0500

    se agrego casi toda la implementacion

--- clojurebb/src/clojurebb/core.clj
+++ clojurebb/src/clojurebb/core.clj
@@ -1,13 +1,13 @@
 (ns clojurebb.core
   (:gen-class)
 )
-
-(defn ball  [x y radius speed direction]	
-		
+(defn ball  [speed direction]	
+	(def xdelta (* speed (Math/cos (Math/toRadians direction))))	
+	(def ydelta (* (- speed) (Math/sin (Math/toRadians direction) )))			
 )
 (defn move [x y xdelta ydelta]
- (def x (+ x xdelta))
- (def y (+ y ydelta)) 
+ (def xr (+ x xdelta))
+ (def yr (+ y ydelta))   
 )
 (defn reflectHorizontal [xdelta]
 	(def xdelta (- xdelta 1))
@@ -15,15 +15,29 @@
 (defn reflectVertical [ydelta]
 	(def ydelta (- ydelta 1))	
 )	
-(defn Container []
-
+(defn Container [x y width heigth]
+	(def x2 (+ x (- width 1)))
+	(def y2 (+ y (- heigth 1)))
 )
-(defn collidesWith[]
-
+(defn collidesWith[x y radius x1 x2 y1 y2 xdelta ydelta]
+	(if 
+		(or (<= (- x radius) x1) (>= (+ x radius) x2))
+			(reflectHorizontal [xdelta] )
+	)
+	(if 
+		(or (<= (- y radius) y1) (>= (+ y radius) y2))
+			(reflectVertical [ydelta] )
+	)
 )	
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]  
-	(print (Math/sin 98))
+  (def x1 0)
+  (def x 0)
+  (def y 0)
+  (def y1 0)
+  (def x2 0)
+  (def y2 0)
+  (def xdelta 0)
+  (def ydelta 0)  
+  (move 4 5 3 3)  
 )
-
