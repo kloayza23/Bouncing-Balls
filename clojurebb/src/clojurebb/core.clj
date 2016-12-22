10e561a6dcc3effa5e2b9eec01ef640b1d6d5faf
Kleber Loayza <kloayza23@gmail.com>
2016-12-22 13:54:33 -0500

    se agrega nueva funcionalidad

--- clojurebb/src/clojurebb/core.clj
+++ clojurebb/src/clojurebb/core.clj
@@ -4,33 +4,49 @@
 (defn collidesWithx[x radius x1 x2 xdelta]
 	(if 
 		(or (<= (- x radius) x1) (>= (+ x radius) x2))
-			(- xdelta 1)
+			(- 0 xdelta)
 			(- xdelta 0)			
 	)	
 )
 (defn collidesWithy[y radius y1 y2 ydelta]	
 	(if 
 		(or (<= (- y radius) y1) (>= (+ y radius) y2))
-			(- ydelta 1)
+			(- 0 ydelta)
 			(- ydelta 0)
 			
 	)
 )
-(defn move [x y radius xdelta ydelta niter x1 y1 x2 y2]	
+(defn move [x y radius xdelta ydelta iiter niter x1 y1 x2 y2]	
 	(if
-		(not= niter 0)
+		(not= iiter 0)
 		(println "Ball at (" x "," y ") of velocity (" xdelta "," ydelta ")")
 	)
 	(if
-		(< niter 10)			
-			(move (+ x xdelta) (+ y ydelta) radius 	(collidesWithx x radius x1 x2 xdelta) (collidesWithy y radius y1 y2 ydelta) (+ niter 1) x1 y1 x2 y2) 						
+		(< iiter niter)			
+			(move (+ x xdelta) (+ y ydelta) radius 	
+				(collidesWithx x radius x1 x2 xdelta) 
+				(collidesWithy y radius y1 y2 ydelta) 
+				(+ iiter 1) niter x1 y1 x2 y2
+			) 						
+		(println "End Game")
 	)		
 )	
 (defn -main
   [& args]    
   (def xdelta (* 10 (Math/cos (Math/toRadians 30))))	
-  (def ydelta (* (- 10) (Math/sin (Math/toRadians 30) )))  
-  (move 50 50 5 xdelta ydelta 0 0 0 99 99)  
+  (def ydelta (* (- 10) (Math/sin (Math/toRadians 30))))  
+  (def x 50)
+  (def y 50)
+  (def radius 5)
+  (def iiter 0); iiter = indice de iteraciones
+  (def niter 100); niter = numero de iteraciones
+  (def x1 0)
+  (def y1 0)
+  (def x2 100)
+  (def y2 100)  
+; tengo que cambiar el valor del collides, y que se mantenga cambiado hasta que
+; llegue otra vez a x2 del otro lado.
+  (move x y radius xdelta ydelta iiter niter x1 y1 x2 y2)  
 )
 ;(let [xdelta (if (or (<= (- x radius) x1) (>= (+ x radius) x2))
 ;		list((- xdelta 1) 0)			
